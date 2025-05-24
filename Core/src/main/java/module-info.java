3c3169140af1c85080d68cf08c7e0f682b745bc2
Frederik <frederik.svarer@hotmail.com>
2025-05-24 20:50:40 +0200

    Spring microservice

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,19 +1,18 @@
 module Core {
     requires javafx.controls;
     requires javafx.graphics;
-    requires spring.context;
     requires spring.core;
     requires spring.beans;
-    requires spring.aop;
-    requires spring.expression;
+    requires spring.context;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.boot.starter.web;
     requires CommonBullet;
     requires Common;
     requires Bullet;
     requires Enemy;
     requires Player;
     requires Collision;
-    requires spring.web;
-    requires spring.boot;
     requires Scoring;
     exports dk.sdu.mmmi.cbse.main to javafx.graphics;
     opens dk.sdu.mmmi.cbse.main to spring.core, spring.beans, spring.context;
